589fa08701ad1deb0cb5ced1d3f473d4416d682b
Akhmetov Ilyas <axmetovia@gmail.com>
2020-12-20 17:33:16 +0600

    Add files via upload

--- screens/TabFourScreen.tsx
+++ screens/TabFourScreen.tsx
@@ -1,151 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Image, TextInput} from 'react-native';
-import data from './data';
-import { Transition, Transitioning } from 'react-native-reanimated';
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
 
-const transition = (
-    <Transition.Together>
-      <Transition.In type='fade' durationMs={200} />
-      <Transition.Change />
-      <Transition.Out type='fade' durationMs={200} />
-    </Transition.Together>
-);
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
 
-export default function App() {
-  const [currentIndex, setCurrentIndex] = React.useState(null);
-  const ref = React.useRef();
-
+export default function TabTwoScreen() {
   return (
-      <Transitioning.View
-          ref={ref}
-          transition={transition}
-          style={styles.container}
-      >
-        <View style={{height: 200, flex: 0, flexDirection: 'column', alignItems: 'center'}}>
-          <Image style={styles.message_img} source={{uri: 'https://webuildthemes.com/go/assets/images/demo/user-1.jpg'}}></Image>
-          <View style={{flex: 0, flexDirection: 'column', alignItems: 'center'}}>
-            <Text style={{fontSize: 22, color: '#898989',}}>Имя Фамилия</Text>
-            <Text style={{fontSize: 14, color: '#898989',}}>Должность</Text>
-          </View>
-        </View>
-        <View style={{flex: 0, paddingRight: 10, paddingLeft: 10}}>
-        <StatusBar hidden />
-              <TouchableOpacity
-                  key={1}
-                  onPress={() => {
-                    ref.current.animateNextTransition();
-                    setCurrentIndex(1 === currentIndex ? null : 1);
-                  }}
-                  style={styles.cardContainer}
-                  activeOpacity={0.9}
-              >
-                <View style={[styles.card]}>
-                  <Text style={styles.heading}>Контакты</Text>
-                  {1 === currentIndex && (
-                      <View style={styles.subCategoriesList}>
-                        <Text>Email</Text>
-                          <TextInput
-                              style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 300}}
-                              placeholder="Поиск"
-                          />
-                          <Text>Номер телефон</Text>
-                          <TextInput
-                              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                              placeholder="Поиск"
-                          />
-                      </View>
-                  )}
-                </View>
-              </TouchableOpacity>
-            <TouchableOpacity
-                key={2}
-                onPress={() => {
-                  ref.current.animateNextTransition();
-                  setCurrentIndex(2 === currentIndex ? null : 2);
-                }}
-                style={styles.cardContainer}
-                activeOpacity={0.9}
-            >
-              <View style={[styles.card]}>
-                <Text style={styles.heading}>Настройки</Text>
-                {2 === currentIndex && (
-                    <View style={styles.subCategoriesList}>
-                      <Text>Test</Text>
-                    </View>
-                )}
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-                key={3}
-                onPress={() => {
-                  ref.current.animateNextTransition();
-                  setCurrentIndex(3 === currentIndex ? null : 3);
-                }}
-                style={styles.cardContainer}
-                activeOpacity={0.9}
-            >
-              <View style={[styles.card]}>
-                <Text style={styles.heading}>Уведомления</Text>
-                {3 === currentIndex && (
-                    <View style={styles.subCategoriesList}>
-                      <Text>Test</Text>
-                    </View>
-                )}
-              </View>
-            </TouchableOpacity>
-        </View>
-      </Transitioning.View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab Two</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabTwoScreen.js" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-  cardContainer: {
-    flexGrow: 1,
-  },
-  card: {
-    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  heading: {
-    fontSize: 22,
-    fontWeight: '900',
-    letterSpacing: -2,
-  },
-  body: {
+  title: {
     fontSize: 20,
-    lineHeight: 20 * 1.5,
-    textAlign: 'center',
+    fontWeight: 'bold',
   },
-  subCategoriesList: {
-    marginTop: 20,
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
   },
-  profile_head:
-  {
-    height: 100,
-  },
-  message_img:
-  {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    alignContent: 'center',
-  },
-  message_img_small:
-  {
-    width: 20,
-    height: 20,
-    borderRadius: 40,
-  },
-  message_text:
-  {
-    width: 50,
-    height: 50,
-  }
 });
